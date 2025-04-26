c71739a3b82bc8cb6efacfd30884755d1d31dbae
Alex-chernomor <68350779+Alex-chernomor@users.noreply.github.com>
2025-04-26 22:23:25 +0200

    corrected

--- ts-home-work-1/src/basic/7.ts
+++ ts-home-work-1/src/basic/7.ts
@@ -10,7 +10,7 @@ type Page = {
 }
 
 
-const page1 = {
+const page1:Page = {
     title: 'The awesome page',
     likes: 100,
     accounts: ['Max', 'Anton', 'Nikita'],
@@ -21,7 +21,7 @@ const page1 = {
     }
   }
   
-  const page2 = {
+  const page2:Page = {
     title: 'Python or Js',
     likes: 5,
     accounts: ['Alex'],
